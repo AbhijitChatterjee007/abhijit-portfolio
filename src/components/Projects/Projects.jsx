eb7e928857df86325ab9a5f87121dd899bcad0ba
AbhijitChatterjee007 <abhijit.chatterjee.5200@gmail.com>
2020-11-15 19:06:19 +0530

    removing duplicate button

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -121,13 +121,6 @@ const Projects = () => {
             );
           })}
         </div>
-        <Fade bottom duration={1000} delay={1000} distance="30px">
-          <p className="hero-cta justify-content-center">
-            <Link className="cta-btn cta-btn--hero" to="/">
-              Go back
-              </Link>
-          </p>
-        </Fade>
       </Container>
     </section>
   );
